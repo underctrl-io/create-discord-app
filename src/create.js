c4ee2208450a76d02fd7985bf13a28513a0ffa74
ZYROUGE <me@zyrouge.com>
2020-10-28 13:10:16 +0530

    Added nice loader instead of linebreaks

--- src/create.js
+++ src/create.js
@@ -2,6 +2,8 @@ const chalk = require("chalk");
 const fs = require("fs");
 const fse = require("fs-extra");
 const cp = require("child_process");
+const ora = require("ora");
+const symbols = require("log-symbols");
 
 class Create {
 
@@ -11,28 +13,29 @@ class Create {
     }
 
     async init(token = null, ops = { language: null }) {
-        if (!this.source) return console.log(chalk.redBright("[Error] No source file(s) specified!"));
+        if (!this.source) return console.log(symbols.error, chalk.redBright("No source file(s) specified!"));
         let path = this.path === "." ? process.cwd() : `${process.cwd()}/${this.path}`;
         if (!fs.existsSync(path)) fs.mkdirSync(path);
 
         fs.readdir(this.source, (error, files) => {
-            if (error) return console.log(chalk.redBright(error.message));
+            if (error) return console.log(symbols.error, chalk.redBright(error.message));
 
-            console.log(chalk.cyanBright("\n\nCopying files..."));
+            const copyFileLoader = ora(chalk.cyanBright("Copying files...")).start();
 
-            files.forEach(file => {
-                fse.copySync(`${this.source}/${file}`, `${path}/${file}`);
-                fs.writeFileSync(path.endsWith("/") ? path + ".env" : path + "/.env", `TOKEN=${token && typeof token === "string" ? token : "ENTER_YOUR_BOT_TOKEN"}`);
-            });
-            console.log(chalk.cyanBright("\nFinished copying files!"));
-            console.log(chalk.blueBright("\nFinalizing..."));
+            for (const file of files) {
+                await fse.copy(`${this.source}/${file}`, `${path}/${file}`);
+                await fse.writeFile(path.endsWith("/") ? path + ".env" : path + "/.env", `TOKEN=${token && typeof token === "string" ? token : "ENTER_YOUR_BOT_TOKEN"}`);
+            }
+
+            copyFileLoader.succeed(chalk.cyanBright("Finished copying files!"));
+            const finalizingLoader = ora(chalk.blueBright("Finalizing...")).start();
 
             const command = this.getInstallCommand(ops.language);
-            if (!command) return console.log(chalk.redBright("Generated project but couldn't install dependencies, please try again manually!"));
+            if (!command) return finalizingLoader.warn(chalk.yellowBright("Generated project but couldn't install dependencies, please try again manually!"));
 
             cp.exec(command, (error) => {
-                if (error) return console.log(chalk.redBright("Generated project but couldn't install dependencies, please try again manually!"));
-                return console.log(chalk.greenBright("\nSuccessfully created discord bot project!"));
+                if (error) return finalizingLoader.warn(chalk.yellowBright("Generated project but couldn't install dependencies, please try again manually!"));
+                return finalizingLoader.succeed(chalk.greenBright("Successfully created discord bot project!"));
             });
         });
     }
